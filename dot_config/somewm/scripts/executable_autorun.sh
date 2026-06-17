36e8d04d109e649c745dbb9793b967414a7217fe
what386 <robot0324@gmail.com>
2026-06-17 12:46:41 -0400

    Update .config/nushell/history.txt Update .config/fish/completions/upstream.fish Update .config/somewm/scripts/autorun.sh

--- dot_config/somewm/scripts/executable_autorun.sh
+++ dot_config/somewm/scripts/executable_autorun.sh
@@ -6,8 +6,6 @@ run() {
     fi
 }
 
-# TODO: fix
-#run "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
 run /usr/lib/polkit-kde-authentication-agent-1
-#run "caffeine-indicator"
-run "${HOME}/.upstream/archives/ollama-linux-amd64/bin/ollama" serve
+run blueman-applet
+#run "${HOME}/.upstream/symlinks/ollama" serve
